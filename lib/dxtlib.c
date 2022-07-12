5d3c46e59a174e1ccd00f9ecf2522155039b3f39
webgtx <dxv1d@tuta.io>
2022-07-12 03:22:41 +0200

    Update: dxtlib fetch update

--- lib/dxtlib.c
+++ lib/dxtlib.c
@@ -9,15 +9,9 @@
 #include <string.h>
 #include <ctype.h>
 
-void matrix();
-void readstr();
 void lnrd();
-void wrt_exmpl();
 void wrt_file();
-void fs_exmpl();
 void rd_file();
-void sleep();
-void foreach();
 int sint(const char *str);
 int system(const char *shell);
 
@@ -43,39 +37,6 @@ void lnrd(char * str) {
   str[idx++] = '\0';
 }
 
-// Only INT, don't trying use arrays with char
-void foreach(int *arr, int len, void (*callback) (int item, int idx)) {
-  unsigned idx = 0;
-  for (idx; idx < len; idx++) 
-    callback(arr[idx], idx);
-}
-
-// This function is broken, stopwatcher always late on 1 seconds for iteration.
-void sleep(int timeout) { // FIXME
-  int idx = 0;
-  printf("sleep was started...\n");
-  int time_bp = time(NULL) + timeout;
-  while (time_bp >= time(NULL)) {
-    printf("%is timeout: %i\n", idx, time(NULL));
-    idx++;
-  }
-}
-
-// this function for test, dont use it
-void readstr(char * string) {
-  scanf("%s", string);
-  printf("STR: %s\nADRS: %p\n", string, string);
-}
-
-void matrix() {
-  char mtx[2][3] = {{"mat"}, {"rix"}};
-  mtx[1][1] = '1';
-  for (int i = 0; i < 2; i++) {
-    printf("%i)idx: [%c] [%c] [%c]\n", i, mtx[i][0], mtx[i][1], mtx[i][2]);
-  }
-  printf("\tsizeof arr = %i bytes\n", sizeof(mtx));
-}
-
 void wrt_file(char * dat, char * filename) { 
   FILE *file = fopen(filename, "w");
     fprintf(file, dat);
@@ -95,4 +56,3 @@ void rd_file(char * dat, char * filename) {
     fclose(file);
   }
 }
-
